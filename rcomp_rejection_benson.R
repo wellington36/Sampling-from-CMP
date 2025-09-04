6353571c2111d915c73bc83880ce2a591c479a9f
Wellington Silva <wellington.71319@gmail.com>
2025-09-04 05:55:31 -0300

    Fix typo

--- rcomp_rejection_benson.R
+++ rcomp_rejection_benson.R
@@ -1,21 +1,11 @@
 source("com_poisson_pmf.R")
 
 # Auxiliar functions
-g <- function(y, gamma, nu) {
-  if (nu < 1) {
-    return(gamma*(1 - gamma)^y)
-  } else {
-    lresult <- y * log(gamma) - gamma - lgamma(y + 1)
-    return(exp(lresult))
-  }
-}
-
 log_B <- function(mu, nu, p = 1) {
   if (nu < 1) {
     a <- floor(mu / (1 - p)^(1/nu))
     lresult <- (nu * a) * log(mu)
       - a * log(1 - p) - nu * lgamma(a + 1) - log(p)
-
     return(lresult)
   } else {
     lresult <- (nu - 1) * (floor(mu) * log(mu) - lgamma(floor(mu) + 1))
@@ -51,7 +41,8 @@ rcomp_rejection_benson <- function(n, lambda, nu) {
     while (i <= n) {
       y_sim <- rgeom(1, p)
       lterm <- y_sim * log(mu) - lgamma(y_sim + 1)
-      log_alpha <- nu * lterm - log_b - lterm
+      log_alpha <- nu * lterm - log_b - log(p) - y_sim * log(1 - p)
+      print(exp(log_alpha))
       u <- runif(1, 0, 1)
 
       if (log(u) < log_alpha) {
@@ -63,5 +54,3 @@ rcomp_rejection_benson <- function(n, lambda, nu) {
   
   return(out)
 }
-
-rcomp_rejection_benson(10, 20, 0.1)
